367a4f41c716b621b0189c92f2782a919e06a6aa
BolshakovDS <ds-8.5@bk.ru>
2026-01-12 22:18:13 +0300

    подготовка вёрстки

---
+++ src/features/ui/UserTableBtn/UserTableBtn.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button} from 'ui-kit-dynamics';
+
+export const UserTableBtn = () => {
+    return (
+        <div>
+            <Button iconName="plus"></Button>
+        </div>
+    );
+};
+
--- src/pages/users/ui/UserPage.tsx
+++ src/pages/users/ui/UserPage.tsx
@@ -1,26 +1,10 @@
-import React, {useEffect} from 'react';
-import {Table} from 'ui-kit-dynamics';
-import {getAllUser} from "../../../entites/user/model/user-thunk";
-import {useAppDispatch, useAppSelector} from "../../../app/store/hooks";
-import {columnsUserTable} from "../const/const";
+import React from 'react';
 
 const UserPage = () => {
-    const dispatch = useAppDispatch();
-    const {users, isLoadingItems} = useAppSelector(state => state.user);
-
-    useEffect(() => {
-        dispatch(getAllUser());
-    }, []);
-
 
     return (
         <section>
 
-            <Table column={columnsUserTable}
-                   title=""
-templateHeader={() => <>Ifgrf</>}
-                   rowKey={'id'}
-                   value={users}/>
         </section>
     );
 };
